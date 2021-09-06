cb26480202da8263b1c42a34f3d96bdf1f68fc47
Ujjal Dey <ujjaldey@gmail.com>
2021-09-06 20:11:19 +0800

    IRPIS-28 The duration input should be human friendly - h:m:s etc

--- app/bot/telegrambothelper.py
+++ app/bot/telegrambothelper.py
@@ -31,11 +31,30 @@ class TelegramBotHelper:
     def _on(self, update: Update, context: CallbackContext):
         self.logger.info('_on is called')
 
-        response_msg = 'OK. Turning the irrigation on...'
+        if len(context.args) < 1:
+            response_msg = 'Enter the duration!'
+            success = False
+        else:
+            duration_str = context.args[0]
+
+            if len(context.args) > 1:
+                duration_str = context.args[0] + context.args[1]
+
+            duration = self.__convert_duration_to_secs(duration_str.lower())
+            print(duration)
+
+            if duration > 0:
+                response_msg = f'OK. Turning the irrigation on for {self.__convert_secs_to_human_format(duration)}...'
+                success = True
+            else:
+                response_msg = 'Invalid duration!'
+                success = False
+
         context.bot.send_message(chat_id=self.config.get_telegram_chat_id(), text=response_msg)
 
-        self.mqtt.publish(self.config.get_mqtt_command_topic(),
-                          '{\"sender\": \"IRPIS-RPI\", \"action\": \"ON\", \"duration\": 20}')
+        if success:
+            self.mqtt.publish(self.config.get_mqtt_command_topic(),
+                              f'{{\"sender\": \"IRPIS-RPI\", \"action\": \"ON\", \"duration\": {duration}}}')
 
     def _off(self, update: Update, context: CallbackContext):
         self.logger.info('_off is called')
@@ -47,3 +66,38 @@ class TelegramBotHelper:
 
     def _send_response(self, message):
         self.updater.bot.send_message(chat_id=self.config.get_telegram_chat_id(), text=message)
+
+    @staticmethod
+    def __convert_duration_to_secs(duration_str):
+        count = duration_str.count(':')
+
+        if count == 0:
+            m = int(duration_str) if duration_str.isnumeric() else 0
+            duration = m * 60
+        elif count == 1:
+            m_str = duration_str.split(":")[0]
+            s_str = duration_str.split(":")[1]
+            m = int(m_str) if m_str.isnumeric() else 0
+            s = int(s_str) if s_str.isnumeric() else 0
+            duration = m * 60 + s
+        elif count == 2:
+            h_str = duration_str.split(":")[0]
+            m_str = duration_str.split(":")[1]
+            s_str = duration_str.split(":")[2]
+            h = int(h_str) if h_str.isnumeric() else 0
+            m = int(m_str) if m_str.isnumeric() else 0
+            s = int(s_str) if s_str.isnumeric() else 0
+            duration = h * 3600 + m * 60 + s
+        else:
+            duration = -1
+
+        return duration
+
+    @staticmethod
+    def __convert_secs_to_human_format(seconds):
+        m, s = divmod(seconds, 60)
+        h, m = divmod(m, 60)
+
+        return ((str(h) + ' hr ' if h > 0 else '') +
+                (str(m) + ' min ' if m > 0 else '') +
+                (str(s) + ' sec ' if s > 0 else '')).strip()
