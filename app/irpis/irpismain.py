27de5a4ff4c6ff2bd5fe51d2028a41adebaa2203
Ujjal Dey <ujjaldey@gmail.com>
2021-09-18 01:36:33 +0800

    IRPIS-50 Code cleanup

--- app/irpis/irpismain.py
+++ app/irpis/irpismain.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from app.irpis.irpismainhelper import IrpisMainHelper
 
 
@@ -15,4 +17,16 @@ class IrpisMain(IrpisMainHelper):
         self.mqtt_client = mqtt_client
 
     def start(self):
-        self._main()
+        counter = 0
+        while True:
+            self.logger.info("Calling IRPIS main")
+            # TODO should be checked at certain interval only
+            self.display.set_wifi_online(self._is_internet_connected())
+
+            if counter >= 20:
+                duration = 5
+                self.mqtt_client.turn_on_payload(duration)
+                counter = 0
+
+            counter += 1
+            sleep(1)
--- app/irpis/irpismainhelper.py
+++ app/irpis/irpismainhelper.py
@@ -1,5 +1,4 @@
 import subprocess
-from time import sleep
 
 # TODO configuration in .env
 PING_URL = '1.1.1.1'
@@ -7,26 +6,6 @@ PING_URL = '1.1.1.1'
 
 class IrpisMainHelper:
     @staticmethod
-    def __is_internet_connected():
+    def _is_internet_connected():
         command = ['ping', '-c', '1', PING_URL]
         return subprocess.call(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) == 0
-
-    def _main(self):
-        on = True
-        counter = 0
-        while True:
-            self.logger.info("Calling IRPIS main")
-            # TODO should be checked at certain interval only
-            self.display.set_wifi_online(self.__is_internet_connected())
-
-            # self.display.enable_backlight(on)
-            # sleep(5 if on else 30)
-            # on = not on
-
-            if counter >= 20:
-                duration = 5
-                self.mqtt_client.turn_on_payload(duration)
-                counter = 0
-
-            counter += 1
-            sleep(1)
